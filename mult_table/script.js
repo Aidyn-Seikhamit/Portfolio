d91c7a90014cd9c4f01d78f162ed903790dba5a0
Aidyn-Seikhamit <Aidynsuperchuvak@gmail.com>
2025-06-28 17:40:01 +0500

    Сделал статистическую часть проекта "Таблица".

--- mult_table/script.js
+++ mult_table/script.js
@@ -1,19 +1,30 @@
 "use strict";
 
-let mainDiv = document.createElement("div");
+const mainDiv = document.createElement("div");
 mainDiv.setAttribute("id", "mainDiv");
 document.body.append(mainDiv);
 
-let blockDiv;
-for (let i= 0; i < 121; i++) {
-    blockDiv = document.createElement("div");
-    blockDiv.setAttribute("id", "block");
-
-    document.querySelector('#mainDiv').append(blockDiv);
-}
-
-let elements = document.querySelectorAll('#block');
-elements.
 for (let i= 0; i < 11; i++) {
+    for (let j = 0; j < 11; j++) {
+        const blockDiv = document.createElement("div");
+        blockDiv.classList.add("block");
+        mainDiv.append(blockDiv);
 
+        if ((i === 0) || (j === 0)){
+            blockDiv.classList.add("block-blue");
+        } else if (i === j){
+            blockDiv.classList.add("block-yellow");
+        } else if (j < i){
+            blockDiv.classList.add("block-brown");
+        } else {
+            blockDiv.classList.add("block-grey");
+        }
+
+        blockDiv.textContent = `${ (i * j) || (i || j) }`;
+
+        // blockDiv.textContent = `${i} / ${j}`;
+    }
 }
+
+
+
