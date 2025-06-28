e4d2cc11370a58fe90186fc2764de688c9238551
Aidyn-Seikhamit <Aidynsuperchuvak@gmail.com>
2025-06-28 16:25:37 +0500

    Первый коммит

---
+++ mult_table/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let mainDiv = document.createElement("div");
+mainDiv.setAttribute("id", "mainDiv");
+document.body.append(mainDiv);
+
+let blockDiv;
+for (let i= 0; i < 121; i++) {
+    blockDiv = document.createElement("div");
+    blockDiv.setAttribute("id", "block");
+
+    document.querySelector('#mainDiv').append(blockDiv);
+}
+
+let elements = document.querySelectorAll('#block');
+elements.
+for (let i= 0; i < 11; i++) {
+
+}
